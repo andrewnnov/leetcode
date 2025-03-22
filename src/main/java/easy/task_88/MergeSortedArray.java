cf0410a1fe58be2c83d98546e11633ee10db2a01
andrewnnov <andrewnnov@yandex.ru>
2025-03-22 17:33:20 +0300

    Merge Sorted Array

--- src/main/java/easy/task_88/MergeSortedArray.java
+++ src/main/java/easy/task_88/MergeSortedArray.java
@@ -5,9 +5,8 @@ public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m - 1;
         int p2 = n - 1;
-        int p = m + n  - 1;
+        int p = m + n - 1;
 
-        // while both arrays have elements to compare
         while (p1 >= 0 && p2 >= 0) {
             if (nums1[p1] > nums2[p2]) {
                 nums1[p] = nums1[p1];
@@ -18,9 +17,8 @@ public class MergeSortedArray {
             }
             p--;
         }
-
         while (p2 >= 0) {
-            nums1[p] = nums2[p2];
+            nums1[p] = nums2[p];
             p2--;
             p--;
         }
